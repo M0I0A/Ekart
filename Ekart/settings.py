48b1d200a4df35eb77d9c9e27dc68aa44a408871
Muhammed Irshad Ali K K <75845752+M0I0A@users.noreply.github.com>
2024-11-10 00:48:53 +0530

    Add Render domain to ALLOWED_HOSTS

--- Ekart/settings.py
+++ Ekart/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-y)-^2agrq&bz^ttk+qoi1xsr3do#+6suy5jcb44n9mc_lbzu9i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://ekart-ycu3.onrender.com']
 
 
 # Application definition
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "/static/"
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
