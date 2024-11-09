6ddc8c3eb07d7a51d9fa6bf6ebbaca5a6c67691e
Muhammed Irshad Ali K K <75845752+M0I0A@users.noreply.github.com>
2024-11-10 01:19:48 +0530

    add static

--- Ekart/settings.py
+++ Ekart/settings.py
@@ -119,14 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "ekart_files/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
-STATICFILES_DIRS=[
-    BASE_DIR/ "static"
-]
 MEDIA_URL='media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
 
