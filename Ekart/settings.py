319981e5f9b353793492533bfbdd1480133f2e9e
Muhammed Irshad Ali K K <75845752+M0I0A@users.noreply.github.com>
2024-11-10 01:03:25 +0530

    Changed Bad req

--- Ekart/settings.py
+++ Ekart/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-y)-^2agrq&bz^ttk+qoi1xsr3do#+6suy5jcb44n9mc_lbzu9i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://ekart-ycu3.onrender.com']
 
 
 # Application definition
